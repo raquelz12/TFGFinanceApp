7105b864535fb796b7ee440ddd23bde5890923f9
raquelz12 <69317735+raquelz12@users.noreply.github.com>
2026-04-20 13:47:27 +0200

    Driver changes

--- selenium_tests/driver.py
+++ selenium_tests/driver.py
@@ -5,7 +5,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def get_driver():
     options = Options()
-    options.add_argument("--start-maximized")
+
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
