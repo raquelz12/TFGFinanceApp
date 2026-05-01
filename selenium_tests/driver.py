daecb4f865e919593939bc39da734a76c16d23ab
raquelz12 <69317735+raquelz12@users.noreply.github.com>
2026-05-01 12:03:15 +0200

    driver headless fix

--- selenium_tests/driver.py
+++ selenium_tests/driver.py
@@ -6,10 +6,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 def get_driver():
     options = Options()
 
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
