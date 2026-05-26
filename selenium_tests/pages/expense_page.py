56c4744674acf249345b28ceb13462bed7dc38c2
raquelz12 <raquelz.12204@gmail.com>
2026-05-26 19:13:24 +0200

    Fix 2

--- selenium_tests/pages/expense_page.py
+++ selenium_tests/pages/expense_page.py
@@ -73,6 +73,7 @@ class ExpensesPage:
         save_btn = self.wait.until(EC.element_to_be_clickable(self.EDIT_SUBMIT_BTN))
         save_btn.click()
         
+        time.sleep(0.5)
         self.wait.until(
             EC.invisibility_of_element_located(self.EDIT_SUBMIT_BTN)
         )
