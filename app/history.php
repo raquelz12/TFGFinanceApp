89d8bb0b216c27e074710de131c3fa8da34f2187
raquelz12 <69317735+raquelz12@users.noreply.github.com>
2026-05-26 18:21:34 +0200

    Prueba 3

--- app/history.php
+++ app/history.php
@@ -86,7 +86,7 @@ include 'includes/header.php';
                         >
                             Ver detalle
                         </button>
-                        <button type="button">Prueba 2</button>
+                        <button type="button">Prueba 3</button>
                     </div>
                 </div>
                 <?php endforeach; ?>
