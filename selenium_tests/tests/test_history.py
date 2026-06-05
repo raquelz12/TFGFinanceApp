a99aaba1c951b5efb9edb2b13311e8c674f5defd
raquelz12 <69317735+raquelz12@users.noreply.github.com>
2026-06-05 12:10:12 +0200

    Test final

--- selenium_tests/tests/test_history.py
+++ selenium_tests/tests/test_history.py
@@ -42,8 +42,6 @@ def test_view_detail(logged_user):
         page.click_first_detail()
         assert "history_detailed" in logged_user.current_url, f"No se ha redirigido a la página de detalle del historial"
 
-    expenses.delete_expense("Gasto Historial")
-
 def test_dead_buttons(logged_user):
     page = HistoryPage(logged_user)
     page.open()
@@ -71,3 +69,5 @@ def test_export_csv_button_exists(logged_user):
 
     export_btn = logged_user.find_elements(By.ID, "exportCsvBtn")
     assert len(export_btn) > 0, "El botón 'Exportar CSV' no existe en la página de historial"
+
+    expenses.delete_expense("Gasto Historial")
