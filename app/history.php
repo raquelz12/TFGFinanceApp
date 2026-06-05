11709acd4c168042aea39ea6edf61d07155cfc08
raquelz12 <69317735+raquelz12@users.noreply.github.com>
2026-06-05 11:57:39 +0200

    Test

--- app/history.php
+++ app/history.php
@@ -63,6 +63,7 @@ include 'includes/header.php';
         <button id="exportCsvBtn" class="button-app" onclick="window.location.href='backend/expenses/export.php'">
             Exportar CSV
         </button>
+        Test
     </section>
     <section class="history-section">
         <?php if (empty($history)): ?>
