f8077a6f5fce14de9893e57c17edd9041294dea0
raquelz12 <69317735+raquelz12@users.noreply.github.com>
2026-06-24 15:47:05 +0200

    Final driver fix

--- selenium_tests/driver.py
+++ selenium_tests/driver.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -5,14 +6,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def get_driver():
     options = Options()
-
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
-    options.binary_location = "/usr/bin/chromium"
-    service = Service("/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=service, options=options)
+    if os.path.exists("/usr/bin/chromedriver"):
+        options.binary_location = "/usr/bin/chromium"
+        service = Service("/usr/bin/chromedriver")
+        driver = webdriver.Chrome(service=service, options=options)
+    else:
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
 
     return driver
